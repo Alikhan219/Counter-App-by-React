6489dbc5e3f0c17b08fe702764c508bf2a69863f
unknown <merabhai786786@gmail.com>
2020-10-15 03:46:47 -0700

    creating the counter application by react and styling the app"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+const App = ()=>{
+
+return(
+  <div className="main_div">
+    <div className="center_div">
+      <h1>0</h1>
+      <div className="btn-div">
+        <button> Increm </button>
+        <button> Decrem </button>
+      </div>
     </div>
-  );
-}
+  </div>
+)
 
+}
 export default App;
