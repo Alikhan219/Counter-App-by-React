83c196d17df8d1f5ed1d96aa7a6139ba5d7a0a56
unknown <merabhai786786@gmail.com>
2020-10-15 04:07:01 -0700

    adding the functionality of incrementing the counting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 
 
-const App = ()=>{
-
-return(
-  <div className="main_div">
-    <div className="center_div">
-      <h1>0</h1>
-      <div className="btn-div">
-        <button> Increm </button>
-        <button> Decrem </button>
-      </div>
-    </div>
-  </div>
-)
+const App = () => {
+    const [num, setnum] = useState(0)
+    const inc=()=>{
+        setnum(num + 1)
+    }
+    return (
+        <div className="main_div">
+            <div className="center_div">
+                <h1> {num} </h1>
+                <div className="btn-div">
+                    <button onClick={inc}> Increm </button>
+                    <button > Decrem </button> </div> </div> </div>
+    )
 
 }
 export default App;
